e5be83eec7f7f7e5ece710cd31c305681aa8da23
Paxton <paxton0505tw@gmail.com>
2024-03-01 02:58:36 +0800

    ✅ update testcases

--- tests/test_currency_converter.py
+++ tests/test_currency_converter.py
@@ -1,5 +1,5 @@
 import unittest
-from models.currency_converter import CurrencyConverter
+from models.currency_converter import CurrencyConverter, InvalidAmountError
 from models.exchange_rate import ExchangeRate
 
 
@@ -19,22 +19,26 @@ class TestCurrencyConverter(unittest.TestCase):
         converter = CurrencyConverter(self.exchange_rate)
         
         # Test with custom exchange rates
-        result = converter.convert("TWD", "JPY", "1000")
+        result = converter.convert("TWD", "JPY", "$1000")
         self.assertEqual(result, "$3,333.30")
 
-        result = converter.convert("JPY", "USD", "10000")
+        result = converter.convert("JPY", "USD", "$10000")
         self.assertEqual(result, "$100.00")
 
-        result = converter.convert("TWD", "USD", "300")
+        result = converter.convert("TWD", "USD", "$300")
         self.assertEqual(result, "$9.00")
 
         # Test with a negative amount
-        result = converter.convert("JPY", "USD", "-100")
+        result = converter.convert("JPY", "USD", "-$100")
         self.assertEqual(result, "-$1.00")
 
         # Test with a zero amount
-        result = converter.convert("TWD", "USD", "0")
+        result = converter.convert("TWD", "USD", "$0")
         self.assertEqual(result, "$0.00")
+
+        # Test invalid amount
+        with self.assertRaises(InvalidAmountError):
+            converter.convert("EUR", "USD", "100")
 
 
 if __name__ == '__main__':
