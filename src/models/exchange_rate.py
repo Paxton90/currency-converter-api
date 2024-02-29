53a48f55cb3fc05e5a2fc822e16f55c25adf5688
Paxton <paxton0505tw@gmail.com>
2024-03-01 03:32:36 +0800

    🔧 update to specified exchange rate

--- src/models/exchange_rate.py
+++ src/models/exchange_rate.py
@@ -1,17 +1,21 @@
 class InvalidCurrencyCode(Exception):
     pass
 
+
 class ExchangeRate:
     DEFAULT_RATES = {
-        "USD": {"USD": 1, "JPY": 3.669, "TWD": 30.444},
-        "JPY": {"USD": 0.00885, "JPY": 1, "TWD": 0.26956},
-        "TWD": {"USD": 0.03281, "JPY": 3.7066, "TWD": 1},
+        "TWD": {"TWD": 1, "JPY": 3.669, "USD": 0.03281},
+        "JPY": {"TWD": 0.26956, "JPY": 1, "USD": 0.00885},
+        "USD": {"TWD": 30.444, "JPY": 111.801, "USD": 1},
     }
 
     def __init__(self, exchange_rates=None):
         self.exchange_rates = exchange_rates or self.DEFAULT_RATES
 
     def get_rate(self, source: str, target: str) -> float:
-        if source not in self.exchange_rates or target not in self.exchange_rates[source]:
+        if (
+            source not in self.exchange_rates
+            or target not in self.exchange_rates[source]
+        ):
             raise InvalidCurrencyCode(f"Invalid currency code: {source} or {target}")
         return self.exchange_rates[source][target]
