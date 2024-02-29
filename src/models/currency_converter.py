4be3c13cb7e3c8ecd62ae0fdfb104aaa73a85384
Paxton <paxton0505tw@gmail.com>
2024-03-01 01:50:41 +0800

    🐛 fix negative symbol dislocation

--- src/models/currency_converter.py
+++ src/models/currency_converter.py
@@ -6,10 +6,12 @@ class CurrencyConverter:
     def __init__(self, exchange_rate: ExchangeRate):
         self.exchange_rate = exchange_rate
 
-    def convert(self, source: str, target: str, amount: str) -> dict:
+    def convert(self, source, target, amount) -> dict:
         if amount.startswith("$"):
+            is_negative = amount.startswith("-")
             amount = Decimal(amount[1:].replace(',', ''))
         else:
+            is_negative = amount.startswith("-")
             amount = Decimal(amount)
 
         rate = Decimal(self.exchange_rate.get_rate(source, target))
@@ -18,4 +20,7 @@ class CurrencyConverter:
         converted_amount_rounded = converted_amount.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
         converted_amount_formatted = "{:,.2f}".format(converted_amount_rounded)
 
-        return "${}".format(converted_amount_formatted)
+        if is_negative:
+            return "-${}".format(converted_amount_formatted[1:])
+        else:
+            return "${}".format(converted_amount_formatted)
