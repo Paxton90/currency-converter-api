9110b6b3fa7b79597e871a787530a68f7c695109
Paxton <paxton0505tw@gmail.com>
2024-03-01 01:14:31 +0800

    🎨 make ExchangeRate testable

--- src/models/exchange_rate.py
+++ src/models/exchange_rate.py
@@ -1,23 +1,12 @@
 class ExchangeRate:
-    def __init__(self):
-        # Initialize the exchange rate data or fetch from other source
-        self.exchange_rates = {
-            "USD": {
-                "USD": 1,
-                "JPY": 3.669,
-                "TWD": 30.444
-            },
-            "JPY": {
-                "USD": 0.00885,
-                "JPY": 1,
-                "TWD": 0.26956
-            },
-            "TWD": {
-                "USD": 0.03281,
-                "JPY": 3.7066,
-                "TWD": 1
-            },
-        }
+    DEFAULT_RATES = {
+        "USD": {"USD": 1, "JPY": 3.669, "TWD": 30.444},
+        "JPY": {"USD": 0.00885, "JPY": 1, "TWD": 0.26956},
+        "TWD": {"USD": 0.03281, "JPY": 3.7066, "TWD": 1},
+    }
+
+    def __init__(self, exchange_rates=None):
+        self.exchange_rates = exchange_rates or self.DEFAULT_RATES
 
     def get_rate(self, source: str, target: str) -> float:
         return self.exchange_rates[source][target]
