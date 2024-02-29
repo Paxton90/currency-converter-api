42197bb456e1b4cad3ea3168b969301f5007fbf6
Paxton <paxton0505tw@gmail.com>
2024-03-01 03:17:33 +0800

    🔧 update swagger settings

--- src/app.py
+++ src/app.py
@@ -6,7 +6,9 @@ from routes.currency_converter import currency_converter_bp
 app = Flask(__name__)
 app.config['SWAGGER'] = {
     'openapi': '3.0.3',
-    'specs_route': '/api-docs/'
+    'specs_route': '/api-docs/',
+    'title': "Currency Converter API Schema - OpenAPI 3.0",
+    'version': '1.0'
 }
 Swagger(app)
 
